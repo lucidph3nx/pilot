954a7cea668c84d8823506583dcc49b913306a6b
lucidph3nx <ph3nx11@gmail.com>
2017-08-01 05:41:09 +1200

    fix

--- app/Functions/vdsRoster.js
+++ app/Functions/vdsRoster.js
@@ -1,9 +1,10 @@
 // returns current datetime and object with todays VDS roster per trip
 module.exports = function vdsRoster() {
-let mysql = require('mysql');
+let mysql = require('mssql');
 
 let con = mysql.createConnection({
-  host: 'apaupvdssql01.vta.local:TDW', // '10.44.0.236:49807', // 'APAUPVDSSQL01',
+  host: '10.44.0.236', // '10.44.0.236:49807', // 'APAUPVDSSQL01',
+  port: '49807',
   user: 'WEBSN',
   password: 'TDW@2017',
   database: 'VDS_TDW',
