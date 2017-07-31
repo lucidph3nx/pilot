840670829f661c9a4e283a40677046a1bec02945
lucidph3nx <ph3nx11@gmail.com>
2017-08-01 06:41:25 +1200

    fix

--- app/Functions/vdsRoster.js
+++ app/Functions/vdsRoster.js
@@ -3,12 +3,15 @@
 let mysql = require('mysql');
 
 let con = mysql.createConnection({
-  host: 'APAUPVDSSQL01\\TDW', // '10.44.0.236:49807', // 'APAUPVDSSQL01',
+  host: 'APAUPVDSSQL01', // '10.44.0.236:49807', // 'APAUPVDSSQL01',
   // port: '49807',
   // localAddress: 'APAUPVDSSQL01\\TDW',
   user: 'WEBSN',
   password: 'TDW@2017',
   database: 'VDS_TDW',
+  dialectOptions: {
+    instanceName: 'TDW',
+  },
 });
 
 con.connect(function(err) {
