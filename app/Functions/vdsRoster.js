f1aee80bf03823d374f59e6320cb0595eb1bf343
lucidph3nx <ph3nx11@gmail.com>
2017-08-01 07:38:38 +1200

    try2 sequelize

--- app/Functions/vdsRoster.js
+++ app/Functions/vdsRoster.js
@@ -1,29 +1,45 @@
 // returns current datetime and object with todays VDS roster per trip
 // module.exports = function vdsRoster() {
-let mysql = require('mysql');
-
-let con = mysql.createConnection({
-  host: 'APAUPVDSSQL01', // '10.44.0.236:49807', // 'APAUPVDSSQL01',
-  port: '49807',
-  // localAddress: 'APAUPVDSSQL01\\TDW',
-  user: 'WEBSN',
-  password: 'TDW@2017',
-  database: 'VDS_TDW',
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize('VDS_TDW', 'WEBSN', 'TDW@2017', {
+  host: 'APAUPVDSSQL01',
+  dialect: 'mssql',
   dialectOptions: {
     instanceName: 'TDW',
-    domain: 'APAUPVDSSQL01',
   },
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query('SELECT 1', function(err, result, fields) {
-    if (err) throw err;
-    console.log('connected!!!');
-  });
-});
+sequelize.query('SELECT 1 FROM [dbo].[FiltreGrilleGrpAgentValidite]')
+  .then(function(myTableRows) {
+    console.log(myTableRows);
+  }
+);
+
+
+// let mysql = require('mysql');
+
+// let con = mysql.createConnection({
+//   host: 'APAUPVDSSQL01', // '10.44.0.236:49807', // 'APAUPVDSSQL01',
+//   port: '49807',
+//   // localAddress: 'APAUPVDSSQL01\\TDW',
+//   user: 'WEBSN',
+//   password: 'TDW@2017',
+//   database: 'VDS_TDW',
+//   dialectOptions: {
+//     instanceName: 'TDW',
+//     domain: 'APAUPVDSSQL01',
+//   },
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query('SELECT 1', function(err, result, fields) {
+//     if (err) throw err;
+//     console.log('connected!!!');
+//   });
+// });
 // };
 
-con.end(function(err) {
-  console.log('terminated!!!');
-});
+// con.end(function(err) {
+//   console.log('terminated!!!');
+// });
